e169e01822ea430470906a4a9dda3d348b883d9f
ykob <info@tplh.net>
2024-03-09 12:37:56 +0900

    added alias and server config for vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '~': `${__dirname}/src/`,
+      'styled-system': `${__dirname}/styled-system/`,
+    },
+  },
+  server: {
+    host: true,
+    port: 3000,
+  },
+});
