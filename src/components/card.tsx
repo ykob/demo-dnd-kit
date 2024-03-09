6f0dba3bf99af3408046ab3008fee353dd6f70be
ykob <info@tplh.net>
2024-03-09 12:57:04 +0900

    update card component styles.

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,7 +1,8 @@
-import { ComponentProps } from "react";
-import { css, cx } from "styled-system/css";
+import { ComponentProps } from 'react';
+import { css, cx } from 'styled-system/css';
+import { flex } from 'styled-system/patterns';
 
-type CardProps = ComponentProps<"div"> & {
+type CardProps = ComponentProps<'div'> & {
   title: string;
   description: string;
 };
@@ -9,14 +10,27 @@ type CardProps = ComponentProps<"div"> & {
 export function Card({ className, title, description, ...props }: CardProps) {
   return (
     <div className={cx(styles.container, className)} {...props}>
-      <div className={styles.title}>{ title }</div>
-      <div className={styles.description}>{ description }</div>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.description}>{description}</div>
     </div>
-  )
+  );
 }
 
 const styles = {
-  container: css({}),
-  title: css({}),
+  container: flex({
+    direction: 'column',
+    gap: 2,
+    boxShadow: 'lg',
+    px: 8,
+    pt: 6,
+    pb: 8,
+    rounded: 'md',
+    bg: 'white',
+  }),
+  title: css({
+    lineHeight: 'tight',
+    textStyle: 'xl',
+    fontWeight: '700',
+  }),
   description: css({}),
-}
+};
