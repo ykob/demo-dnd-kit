6cecdad25d8c64d59a98a787d4d840c5f56a77fe
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2024-03-11 19:26:29 +0900

    moved the sortable section to nest folder.

--- src/sections/sortable-section.tsx
+++
@@ -1,9 +0,0 @@
-import { Card, Section } from '~/components/';
-
-export function SortableSection() {
-  return (
-    <Section heading="Sortable">
-      <Card title="Card 1" description="Description 1" />
-    </Section>
-  );
-}
---
+++ src/sections/sortable-section/index.tsx
@@ -0,0 +1,65 @@
+import { DndContext, DragEndEvent } from '@dnd-kit/core';
+import { SortableContext, arrayMove } from '@dnd-kit/sortable';
+import { useState } from 'react';
+import { flex } from 'styled-system/patterns';
+import { Card, Section } from '~/components/';
+
+export function SortableSection() {
+  const [items, setItems] = useState([
+    {
+      id: '1',
+      title: 'Card 1',
+      description: 'Description 1',
+    },
+    {
+      id: '2',
+      title: 'Card 2',
+      description: 'Description 2',
+    },
+    {
+      id: '3',
+      title: 'Card 3',
+      description: 'Description 3',
+    },
+  ]);
+
+  const handleDragEnd = (e: DragEndEvent) => {
+    const { active, over } = e;
+
+    if (!over) return;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.findIndex((item) => item.id === active.id);
+        const newIndex = items.findIndex((item) => item.id === over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  };
+
+  return (
+    <Section heading="Sortable">
+      <div className={styles.cards}>
+        <DndContext onDragEnd={handleDragEnd}>
+          <SortableContext items={items}>
+            {items.map((item) => (
+              <Card
+                key={item.id}
+                title={item.title}
+                description={item.description}
+              />
+            ))}
+          </SortableContext>
+        </DndContext>
+      </div>
+    </Section>
+  );
+}
+
+const styles = {
+  cards: flex({
+    direction: 'column',
+    gap: 2,
+  }),
+};
