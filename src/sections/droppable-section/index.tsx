d954d134cff6a4f57c5b2b60d85055e57ab3e49a
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2024-03-22 18:02:12 +0900

    added draggable and droppable components.

--- src/sections/droppable-section/index.tsx
+++ src/sections/droppable-section/index.tsx
@@ -1,11 +1,37 @@
-import { Section } from "~/components";
+import { DndContext, useDraggable, useDroppable } from '@dnd-kit/core';
+import { CSS } from '@dnd-kit/utilities';
+import { Section } from '~/components';
+
+const Draggable = () => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: 'draggable',
+  });
+  const style = {
+    transform: CSS.Transform.toString(transform),
+  };
+
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      Draggable
+    </div>
+  );
+};
+
+const Droppable = () => {
+  const { isOver, setNodeRef } = useDroppable({
+    id: 'droppable',
+  });
+
+  return <div ref={setNodeRef}>Droppable</div>;
+};
 
 export function DroppableSection() {
   return (
     <Section heading="Droppable">
-      <div>
-        
-      </div>
+      <DndContext>
+        <Droppable />
+        <Draggable />
+      </DndContext>
     </Section>
-  )
+  );
 }
