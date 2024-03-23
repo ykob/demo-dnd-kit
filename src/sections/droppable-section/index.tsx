f76ade4e8f5262194a1636805d67e2d3754ddd1d
ykob <info@tplh.net>
2024-03-23 17:03:27 +0900

    added handleDragEnd event.

--- src/sections/droppable-section/index.tsx
+++ src/sections/droppable-section/index.tsx
@@ -1,10 +1,16 @@
-import { DndContext, useDraggable, useDroppable } from '@dnd-kit/core';
+import {
+  DndContext,
+  DragEndEvent,
+  useDraggable,
+  useDroppable,
+} from '@dnd-kit/core';
 import { CSS } from '@dnd-kit/utilities';
 import { Section } from '~/components';
 
 const Draggable = () => {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: 'draggable',
+    data: { type: 'type1' },
   });
   const style = {
     transform: CSS.Transform.toString(transform),
@@ -20,15 +26,30 @@ const Draggable = () => {
 const Droppable = () => {
   const { isOver, setNodeRef } = useDroppable({
     id: 'droppable',
+    data: {
+      accepts: ['type1', 'type2'],
+    },
   });
 
   return <div ref={setNodeRef}>Droppable</div>;
 };
 
 export function DroppableSection() {
+  function handleDragEnd(e: DragEndEvent) {
+    const { active, over } = e;
+    console.log(active, over);
+
+    if (
+      over &&
+      over.data.current?.accepts.includes(active.data.current?.type)
+    ) {
+      console.log('Dropped draggable into droppable');
+    }
+  }
+
   return (
     <Section heading="Droppable">
-      <DndContext>
+      <DndContext onDragEnd={handleDragEnd}>
         <Droppable />
         <Draggable />
       </DndContext>
